b9673b6d34cadc3297428164984dbc8567ae9358
shrey829 <shahshrey521@gmail.com>
2026-07-28 14:03:48 +0530

    Update shrey.py

--- shrey.py
+++ shrey.py
@@ -8,7 +8,7 @@ sum = 0
 
 match num:
     case 1:
-        print("Rows value should be positive.")
+        
         rows = int(input("Enter the number of rows: "))
 
         if rows > 0:
@@ -17,25 +17,27 @@ match num:
                     print("*", end="")
                 print()
         else:
-            pass
+           print("Rows value should be positive.")
 
     case 2:
-        print("Start of range should be less than end of range.")
+       
         num1 = int(input("Enter the starting number: "))
         num2 = int(input("Enter the ending number: "))
 
         if num2 > num1:
-            for i in range(num1, num2 + 1):
-                if i % 2 == 0:
-                    print(f"Number {i} is even")
-                else:
-                    print(f"Number {i} is odd")
+            for i in range(num2 + 1):
+                if(i<=num1):
+                    pass
+                    if i % 2 == 0:
+                      print(f"Number {i} is even")
+                    else:
+                      print(f"Number {i} is odd")
 
                 sum += i
 
             print("Sum is:", sum)
         else:
-            pass
+             print("Start of range should be less than end of range.")
 
     case 3:
         print("Thank you! Exiting the program... Goodbye!")
